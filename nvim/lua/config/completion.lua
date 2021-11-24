e8ec22afe36c4a6db4a0fc002724a5e3f87ec691
nichijou <zhangbshu@gmail.com>
2021-11-24 22:57:39 +0800

    cmp

--- nvim/lua/config/completion.lua
+++ nvim/lua/config/completion.lua
@@ -40,10 +40,11 @@ M.setup = function()
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        -- elseif vim.fn["vsnip#available"](1) == 1 then
-        --   feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif vim.fn["vsnip#available"](1) == 1 then
+          feedkey("<Plug>(vsnip-expand-or-jump)", "")
         elseif has_words_before() then
-          cmp.complete()
+          fallback()
+          -- cmp.complete()
         else
           fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
         end
