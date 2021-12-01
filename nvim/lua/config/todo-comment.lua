3aa8787e82195e888a1dba95b2c569d81da8c613
in-a-day <zhangbshu@gmail.com>
2021-12-01 17:11:56 +0800

    todo-comment

--- nvim/lua/config/todo-comment.lua
+++ nvim/lua/config/todo-comment.lua
@@ -1,6 +1,6 @@
 local M = {}
 
--- TODO
+-- INFO:
 M.setup = function ()
   require("todo-comments").setup({
     -- keywords recognized as todo comments
@@ -17,7 +17,30 @@ M.setup = function ()
       PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
       NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
     },
+    highlight = {
+      before = "", -- "fg" or "bg" or empty
+      keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+      after = "", -- "fg" or "bg" or empty
+      pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+      comments_only = true, -- uses treesitter to match keywords in comments only
+      max_line_len = 400, -- ignore lines longer than this
+      exclude = {}, -- list of file types to exclude highlighting
+    },
     merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+    search = {
+      command = "rg",
+      args = {
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+      },
+      -- regex that will be used to match keywords.
+      -- don't replace the (KEYWORDS) placeholder
+      pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+      -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+    },
   })
 end
 
