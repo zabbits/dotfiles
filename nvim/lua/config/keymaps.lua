8437d734fb261a81e51f56a163588ba522ce2506
neozbs <zhangbshu@gmail.com>
2022-04-10 23:56:11 +0800

    nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -55,3 +55,7 @@ vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
 vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+
+-- insert current time
+keymap("n", "<A-i>", ":put =strftime('%Y-%m-%d %H:%M:%S')<CR>", opts)
+
