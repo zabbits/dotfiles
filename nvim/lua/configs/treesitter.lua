7c4dd885adb89234d73732aa045dc2738fc9523a
neozbs <zhangbshu@gmail.com>
2022-05-18 22:49:19 +0800

    change norg ts parser

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -6,26 +6,7 @@ function M.config()
     return
   end
 
-  -- add support for neorg
-  local parser_configs = require "nvim-treesitter.parsers".get_parser_configs()
-  -- These two are optional and provide syntax highlighting
-  -- for Neorg tables and the @document.meta tag
-  parser_configs.norg_meta = {
-    install_info = {
-      url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-      files = { "src/parser.c" },
-      branch = "main"
-    },
-  }
-  parser_configs.norg_table = {
-    install_info = {
-      url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-      files = { "src/parser.c" },
-      branch = "main"
-    },
-  }
-
-  local ensure_installed = { 'lua', 'python', 'vue', 'typescript', 'norg', 'norg_meta', 'norg_table', 'html', 'css', 'go', }
+  local ensure_installed = { 'lua', 'python', 'vue', 'typescript', 'norg', 'html', 'css', 'go', }
 
   local default_opts = {
     ensure_installed = ensure_installed,
