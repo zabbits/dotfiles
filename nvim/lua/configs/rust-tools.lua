6141e11a02e0d0902d1a972f2a69d93ec56fc93b
zbs <zhangbshu@gmail.com>
2022-08-09 09:28:33 +0800

    fix rust-tools config

--- nvim/lua/configs/rust-tools.lua
+++ nvim/lua/configs/rust-tools.lua
@@ -3,13 +3,47 @@ local M = {}
 function M.config()
   local rt = safe_require("rust-tools")
   local handlers = safe_require('configs.lsp.handlers')
-  if not rt then
+  if not rt or not handlers then
     return
   end
   rt.setup({
     tools = {
-      autoSetHints = false,
-      hover_with_actions = true,
+      -- These apply to the default RustSetInlayHints command
+      inlay_hints = {
+        -- automatically set inlay hints (type hints)
+        -- default: true
+        auto = true,
+
+        -- Only show inlay hints for the current line
+        only_current_line = false,
+
+        -- whether to show parameter hints with the inlay hints or not
+        -- default: true
+        show_parameter_hints = true,
+
+        -- prefix for parameter hints
+        -- default: "<-"
+        parameter_hints_prefix = "<- ",
+
+        -- prefix for all the other hints (type, chaining)
+        -- default: "=>"
+        other_hints_prefix = "=> ",
+
+        -- whether to align to the lenght of the longest line in the file
+        max_len_align = false,
+
+        -- padding from the left if max_len_align is true
+        max_len_align_padding = 1,
+
+        -- whether to align to the extreme right or not
+        right_align = false,
+
+        -- padding from the right if right_align is true
+        right_align_padding = 7,
+
+        -- The color of the hints
+        highlight = "Comment",
+      },
       -- options right now: termopen / quickfix
       executor = require("rust-tools/executors").termopen,
     },
