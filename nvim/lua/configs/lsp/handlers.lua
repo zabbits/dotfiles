41edc03165e9d0b8907c6555b5ff3f7b9914c656
zbs <zhangbshu@gmail.com>
2022-10-23 22:24:43 +0800

    update cmp config

--- nvim/lua/configs/lsp/handlers.lua
+++ nvim/lua/configs/lsp/handlers.lua
@@ -109,7 +109,7 @@ M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local cmp_nvim_lsp = _G.safe_require("cmp_nvim_lsp")
 if cmp_nvim_lsp then
-  M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
+  M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 end
 
 -- for fold
