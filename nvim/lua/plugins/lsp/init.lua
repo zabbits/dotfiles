8af6f486b1d1ba0b61eec33c2062ee9f43ffbcee
in-a-day <zhangbshu@gmail.com>
2023-02-06 10:07:42 +0800

    add prettier

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -157,6 +157,7 @@ local null_ls = {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
 			},
 		})
 		require("mason-null-ls").setup({
