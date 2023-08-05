80372c30365924b69dbcd8b0825131d4530b2198
zabbits <zhangbshu@gmail.com>
2023-08-06 00:18:32 +0800

    nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -643,17 +643,11 @@ local test_conf = {
 	end,
 }
 
-local codeium_conf = {
-  'Exafunction/codeium.vim',
-	event = { "BufRead", "BufNewFile" },
-  config = function ()
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-    vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-    vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-    vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-  end
+local yuck_conf = {
+  'elkowar/yuck.vim',
+  ft = 'yuck',
 }
+
 
 return {
 	ts_conf,
@@ -669,5 +663,5 @@ return {
 	surround_conf,
 	table_conf,
 	test_conf,
-  codeium_conf,
+  yuck_conf,
 }
