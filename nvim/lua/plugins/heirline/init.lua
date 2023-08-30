2de2ce60766cee503f9e16f6689170807a10eabd
zabbits <zhangbshu@gmail.com>
2023-08-30 22:38:20 +0800

    nvim

--- nvim/lua/plugins/heirline/init.lua
+++ nvim/lua/plugins/heirline/init.lua
@@ -72,75 +72,97 @@ return {
       hl = hl.Mode.normal,
     }
 
-    local Indicator
-    do
-      local VimMode
-      do
-        local NormalModeIndicator = {
-          Space,
-          {
-            fallthrough = false,
-            ReadOnly,
-            {
-              provider = icons.circle,
-              hl = function()
-                if bo.modified then
-                  -- return { fg = hl.Mode.insert.bg }
-                  return { fg = colors.blue2 }
-                else
-                  return hl.Mode.normal
-                end
-              end,
-            },
-          },
-          Space,
-        }
-
-        local ActiveModeIndicator = {
-          condition = function(self)
-            return self.mode ~= "normal"
-          end,
-          hl = { bg = hl.StatusLine.bg },
-          heirline.surround({ icons.powerline.left_rounded, icons.powerline.right_rounded }, function(self) -- color
-            return hl.Mode[self.mode].bg
-          end, {
-            {
-              fallthrough = false,
-              ReadOnly,
-              { provider = icons.circle },
-            },
-            Space,
-            {
-              provider = function(self)
-                return util.mode_lable[self.mode]
-              end,
-            },
-            hl = function(self)
-              return hl.Mode[self.mode]
-            end,
-          }),
-        }
-
-        VimMode = {
-          init = function(self)
-            self.mode = mode[fn.mode(1)] -- :h mode()
-          end,
-          condition = function()
-            return bo.buftype == ""
-          end,
-          {
-            fallthrough = false,
-            ActiveModeIndicator,
-            NormalModeIndicator,
-          },
-        }
-      end
-
-      Indicator = {
-        fallthrough = false,
-        VimMode,
-      }
-    end
+    local ViMode = {
+      -- get vim current mode, this information will be required by the provider
+      -- and the highlight functions, so we compute it only once per component
+      -- evaluation and store it as a component attribute
+      init = function(self)
+        self.mode = vim.fn.mode(1) -- :h mode()
+      end,
+      -- Now we define some dictionaries to map the output of mode() to the
+      -- corresponding string and color. We can put these into `static` to compute
+      -- them at initialisation time.
+      static = {
+        mode_names = { -- change the strings if you like it vvvvverbose!
+          n = "N",
+          no = "N?",
+          nov = "N?",
+          noV = "N?",
+          ["no\22"] = "N?",
+          niI = "Ni",
+          niR = "Nr",
+          niV = "Nv",
+          nt = "Nt",
+          v = "V",
+          vs = "Vs",
+          V = "Vl",
+          Vs = "Vs",
+          ["\22"] = "^V",
+          ["\22s"] = "^V",
+          s = "S",
+          S = "S_",
+          ["\19"] = "^S",
+          i = "I",
+          ic = "Ic",
+          ix = "Ix",
+          R = "R",
+          Rc = "Rc",
+          Rx = "Rx",
+          Rv = "Rv",
+          Rvc = "Rv",
+          Rvx = "Rv",
+          c = "C",
+          cv = "Ex",
+          r = "...",
+          rm = "M",
+          ["r?"] = "?",
+          ["!"] = "!",
+          t = "T",
+        },
+        mode_colors = {
+          n = "red",
+          i = "green",
+          v = "cyan",
+          V = "cyan",
+          ["\22"] = "cyan",
+          c = "orange",
+          s = "purple",
+          S = "purple",
+          ["\19"] = "purple",
+          R = "orange",
+          r = "orange",
+          ["!"] = "red",
+          t = "red",
+        },
+      },
+      -- We can now access the value of mode() that, by now, would have been
+      -- computed by `init()` and use it to index our strings dictionary.
+      -- note how `static` fields become just regular attributes once the
+      -- component is instantiated.
+      -- To be extra meticulous, we can also add some vim statusline syntax to
+      -- control the padding and make sure our string is always at least 2
+      -- characters long. Plus a nice Icon.
+      provider = function(self)
+        if self.mode == "n" then
+          return ""
+        end
+        return " " .. self.mode_names[self.mode] .. ""
+      end,
+      -- Same goes for the highlight. Now the foreground will change according to the current mode.
+      hl = function(self)
+        local mode_ = self.mode:sub(1, 1) -- get only the first mode character
+        return { fg = self.mode_colors[mode_], bold = true }
+      end,
+      -- Re-evaluate the component only on ModeChanged event!
+      -- Also allows the statusline to be re-evaluated when entering operator-pending mode
+      update = {
+        "ModeChanged",
+        pattern = "*:*",
+        callback = vim.schedule_wrap(function()
+          vim.cmd("redrawstatus")
+        end),
+      },
+    }
 
     local FileNameBlock, CurrentPath, FileName
     do
@@ -357,7 +379,6 @@ return {
             return names
           end,
         },
-        Space(1),
         hl = hl.LspServer,
       }
 
@@ -366,7 +387,7 @@ return {
         init = function(self)
           local names = {}
           for _, server in pairs(vim.lsp.get_clients()) do
-            if (server.name ~= "null-ls") then
+            if server.name ~= "null-ls" then
               table.insert(names, server.name)
             end
           end
@@ -528,7 +549,7 @@ return {
       hl = hl.StatusLine,
       {
         LeftCap,
-        Indicator,
+        ViMode,
         Space,
         Git,
         Diagnostics,
