098d75d1ba3b9602fce51af2f358ed6c3b483f4e
zabbits <zhangbshu@gmail.com>
2023-11-14 21:22:32 +0800

    nvim

--- nvim/lua/base/colorscheme.lua
+++ nvim/lua/base/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
     {
         "rebelot/kanagawa.nvim",
+        priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             require("kanagawa").setup({
                 transparent = true, -- do not set background color
