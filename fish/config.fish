cecd08d40871e224c576b27bacd13bfe478df122
zabbits <zabbits@outlook.com>
2024-12-18 22:17:15 +0800

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias vi=nvim
     alias vim=nvim
-    alias l='lsd -lah'
-    alias ls=lsd
+    alias ls='exa --color=always --icons=always'
     alias hx=helix
 
+    fzf --fish | source
     zoxide init fish | source
     atuin init fish | source
     starship init fish | source
+    mise activate fish | source
 end
