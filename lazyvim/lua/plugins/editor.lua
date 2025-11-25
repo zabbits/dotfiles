9f12ea523ae10f76e847eb2b6a78cc4a579e4f12
zabbits <git@zabbits.com>
2025-11-25 17:48:55 +0800

    nvim

--- lazyvim/lua/plugins/editor.lua
+++ lazyvim/lua/plugins/editor.lua
@@ -11,8 +11,55 @@ return {
         },
     },
     {
+        "A7Lavinraj/fyler.nvim",
+        dependencies = { "nvim-mini/mini.icons" },
+        branch = "stable", -- Use stable branch for production
+        lazy = true,
+        keys = {
+            {
+                "<leader>e",
+                function()
+                    require("fyler").open({ kind = "split_left_most" })
+                end,
+                desc = "Aerial (Symbols)",
+            },
+        },
+        opts = {
+            hooks = {},
+            integrations = {
+                icon = "mini_icons",
+            },
+            views = {
+                finder = {
+                    win = {
+                        border = vim.o.winborder == "" and "single" or vim.o.winborder,
+                        kinds = {
+                            float = {
+                                height = "70%",
+                                width = "70%",
+                                top = "10%",
+                                left = "15%",
+                            },
+                            split_left = {
+                                width = "15%",
+                            },
+                            split_left_most = {
+                                width = "15%",
+                                win_opts = {
+                                    winfixwidth = true,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+    {
         "folke/snacks.nvim",
         keys = {
+            { "<leader>e", false },
+            { "<leader>E", false },
             { "<C-e>", LazyVim.pick("buffers"), desc = "Buffers" },
             { "<leader>/", LazyVim.pick("files", { hidden = true }), desc = "Grep (With Hidden)" },
             { "<leader>:", LazyVim.pick("grep", { hidden = true }), desc = "Find Files (With Hidden)" },
